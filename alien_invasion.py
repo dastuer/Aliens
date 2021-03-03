1b7e9ee64aa699a69163da1989872abc6d96afd7
huaihui's labtop <1438730008@qq.com>
2021-03-03 17:23:51 +0800

    add button

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from ship import Ship
 from alien import Alien
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
 
 
 def run_game():
@@ -12,6 +13,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
@@ -20,12 +22,12 @@ def run_game():
     # print('x:'+str(alien.rect.x) + ' y:'+str(alien.rect.y) + ' centerx:' + str(alien.rect.centerx))
     stats = GameStats(ai_settings)
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
         if stats.game_active :
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_alien(ai_settings, stats, screen, ship, aliens ,bullets )
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+            gf.update_alien(ai_settings, stats, screen, ship, aliens ,bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
